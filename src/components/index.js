926488479691f04525d949a80844d5fc5363866b
Stas Dubinin <dubinin.stas@gmail.com>
2023-01-17 21:06:28 +0400

    fix bugs and refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -1,45 +1,60 @@
-import '../pages/index.css';
-import {enableValidation, validationVar } from './validate.js';
-import {handleProfileFormSubmit} from "./utils.js";
-import {openPopup, closePopup, addListenerPopup, addClosePopupEscape} from "./modal.js";
-import {initialCards, pastNewMesto, handleAddMestoFormSubmit, nameMestoInput, linkMestoInput, popupAddMesto} from "./card.js";
-import {popupEditProfile, profileUserName, profileUserAbout,nameInput, aboutInput} from "./utils.js";
+import "../pages/index.css";
+import { enableValidation, validationVar } from "./validate.js";
+import { openPopup, closePopup, addListenerPopup } from "./modal.js";
+import {
+  pastNewMesto,
+  handleAddMestoFormSubmit,
+  nameMestoInput,
+  linkMestoInput,
+  popupAddMesto,
+  cardsContainer,
+} from "./card.js";
 
+import { initialCards } from "./initial-cards";
 
-const formAddMesto = document.querySelector('form[name="add-new-mesto"');
-const buttonAddMesto = document.querySelector('.profile__add-button');
-const buttonEditProfile = document.querySelector('.profile__edit-button');
-const buttonsClose = document.querySelectorAll('.popup__icon-close');
-const formElement = document.querySelector('form[name="edit-user"]');
+const formAddMesto = document.querySelector("form[name=add-new-mesto]");
+const buttonAddMesto = document.querySelector(".profile__add-button");
+const buttonEditProfile = document.querySelector(".profile__edit-button");
+const buttonsClose = document.querySelectorAll(".popup__icon-close");
+const formEditUser = document.querySelector('form[name="edit-user"]');
+const buttonSaveMesto = popupAddMesto.querySelector(".popup__button-save");
+const profileUserName = document.querySelector(".profile__user-name");
+const profileUserAbout = document.querySelector(".profile__user-about");
+const popupEditProfile = document.querySelector(".profile-popup");
+const nameInput = document.querySelector("input[name=name-user]");
+const aboutInput = document.querySelector("input[name=about-user]");
 
+enableValidation(validationVar);
+addListenerPopup();
 
-enableValidation (validationVar);
-addClosePopupEscape ()
-addListenerPopup ();
+function handleProfileFormSubmit(evt) {
+  //функция заменяющая стандартную отправку формы
+  evt.preventDefault();
+  profileUserName.textContent = nameInput.value;
+  profileUserAbout.textContent = aboutInput.value;
+  closePopup(popupEditProfile);
+}
 
-formElement.addEventListener('submit', handleProfileFormSubmit);
+formEditUser.addEventListener("submit", handleProfileFormSubmit);
 
+buttonsClose.forEach((button) => {
+  button.addEventListener("click", closePopup);
+});
 
-buttonsClose.forEach((button)=>{
-  const popup = button.closest('.popup');
-  button.addEventListener('click', closePopup);
-})
-
-buttonEditProfile.addEventListener('click', () =>{
+buttonEditProfile.addEventListener("click", () => {
   nameInput.value = profileUserName.textContent;
   aboutInput.value = profileUserAbout.textContent;
-  openPopup (popupEditProfile)
-
+  openPopup(popupEditProfile);
 });
 
-buttonAddMesto.addEventListener('click', () =>{
-  nameMestoInput.value = '';
-  linkMestoInput.value ='';
-  openPopup (popupAddMesto)
+buttonAddMesto.addEventListener("click", () => {
+  nameMestoInput.value = "";
+  linkMestoInput.value = "";
+  openPopup(popupAddMesto);
 });
 
-initialCards.forEach((elem)=>{pastNewMesto(elem.name, elem.link)});
+initialCards.forEach((elem) => {
+  pastNewMesto(elem.name, elem.link, cardsContainer);
+});
 
-formAddMesto.addEventListener('submit', handleAddMestoFormSubmit);//слушатель отправки формы добавления нового места
-
-
+formAddMesto.addEventListener("submit", handleAddMestoFormSubmit); //слушатель отправки формы добавления нового места
